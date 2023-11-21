0f417b4ae7680c790d053793379613511133e1ff
João Morais <127213606+joaomoraiss@users.noreply.github.com>
2023-11-21 00:27:12 -0300

    Update NotFound.jsx
    a

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -12,17 +12,4 @@ const NotFound = () => {
 };
 
 
-// NotFound.jsx
-import React from 'react';
-
-const NotFound = () => {
-  return (
-    <div>
-      <h1>404 - Página não encontrada</h1>
-      <p>Desculpe, a página que você está procurando não existe.</p>
-    </div>
-  );
-};
-
-
 export default NotFound;
