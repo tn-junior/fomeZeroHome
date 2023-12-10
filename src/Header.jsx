948a4eaaff789666c57f742498a256bbf0e06359
joaomoraiss <joaovictorrmpework@outlook.com>
2023-12-10 12:53:04 -0300

    Sidebar completa

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Flex, Box, Input, Button, useDisclosure, Collapse,  } from '@chakra-ui/react';
+import React, { useRef } from 'react';
+import { Flex, Box, Input, Button, useDisclosure, Collapse } from '@chakra-ui/react';
 import MobileMenu from './MobileMenu';
-import { Search2Icon, EmailIcon   } from '@chakra-ui/icons'
-import { IconButton } from '@chakra-ui/react'
-import { RiHome3Line } from "react-icons/ri";
+import { Search2Icon, EmailIcon } from '@chakra-ui/icons';
+import { IconButton } from '@chakra-ui/react';
+import { RiHome3Line } from 'react-icons/ri';
 import Sidebar from './Sidebar';
 
- function header( ) {
+function Header() {
   const { isOpen, onToggle } = useDisclosure();
   const [isSearchFocused, setIsSearchFocused] = React.useState(false);
+
+  const inputRef = useRef(null);
 
   const handleSearchFocus = () => {
     setIsSearchFocused(true);
@@ -17,61 +19,82 @@ import Sidebar from './Sidebar';
   const handleSearchBlur = () => {
     setIsSearchFocused(false);
   };
-  return (
-    <Flex top="16" as="header" align="center" justifyContent="space-between" p={2} bg="#F6AD55" color="white">
-    <Box  >
-    <Button ml={2} p={2} _hover={{ bg: '#ED8936' }} h={12}  colorScheme="" mr={4}>
-      <IconButton _hover={{ bg: 'none' }} h={8}  bg = "none" icon as={RiHome3Line} />
-    </Button>  
-    </Box>
 
-    <Box zIndex={2} >
-    <Button colorScheme="" mr={4}>
-    <Input
-          borderColor={isSearchFocused ? 'black' : 'black'}
-          borderWidth={1.6}
-          _placeholder={{ opacity: 0.6, color: 'black' }}
-          _focus={{ borderColor: 'black', boxShadow:'none' }}
-          _hover={{ borderColor: 'black' }}
-          onFocus={handleSearchFocus}
-          borderRadius={4}
-          color="black"
-          onBlur={handleSearchBlur}
-          placeholder="Pesquisar..."
-          w={500}
-          mr={2}  
-          
-        />
-      <IconButton  _hover={{ bg: '#ED8936' }} bg = "none"  wd aria-label='Search database' icon={<Search2Icon />} />
-    </Button>  
-    </Box>
+  const handleContatoClick = () => {
+    const enderecoEmail = 'fomezero@outlook.com'; 
 
    
-    <Box marginRight={20} display={{ base: 'none', md: 'block' }}>
+    const tempInput = document.createElement('input');
+    tempInput.value = enderecoEmail;
+
     
-      <Button color='black' _hover={{ bg: '#ED8936' }} bg = "none" colorScheme="teal" mr={4}>
-        Início
-      </Button>
-      <Button color = 'black' _hover={{ bg: '#ED8936' }}  bg = "none" colorScheme="teal" mr={4}>
-        Sobre
-      </Button>
-      <Button color = 'black' _hover={{ bg: '#ED8936' }} leftIcon={<EmailIcon />} align="center" bg = "none" colorScheme="teal" mr={1}>
-        Contato
-      </Button>
-    </Box >
+    document.body.appendChild(tempInput);
 
-    <Box display={{ base: 'block', md: 'none' }}>
-      <Button onClick={onToggle} colorScheme="teal">
-        Menu
-      </Button>
-    </Box>
+   
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); 
 
-    <Collapse in={isOpen}>
-      <MobileMenu />
-    </Collapse>
-      <Sidebar/>
-  </Flex>
-  
-);
+    
+    document.execCommand('copy');
+
+    // Remove o campo de entrada da DOM
+    document.body.removeChild(tempInput);
+
+    console.log('Endereço de e-mail copiado para a área de transferência:', enderecoEmail);
+  };
+
+  return (
+    <Flex top="16" as="header" align="center" justifyContent="space-between" p={2} bg="#F6AD55" color="white">
+      <Box>
+        <Button ml={2} p={2} _hover={{ bg: '#ED8936' }} h={12} colorScheme="" mr={4}>
+          <IconButton _hover={{ bg: 'none' }} h={8} bg="none" icon as={RiHome3Line} />
+        </Button>
+      </Box>
+
+      <Box zIndex={2}>
+        <Button colorScheme="" mr={4}>
+          <Input
+            borderColor={isSearchFocused ? 'black' : 'black'}
+            borderWidth={1.6}
+            _placeholder={{ opacity: 0.6, color: 'black' }}
+            _focus={{ borderColor: 'black', boxShadow: 'none' }}
+            _hover={{ borderColor: 'black' }}
+            onFocus={handleSearchFocus}
+            borderRadius={4}
+            color="black"
+            onBlur={handleSearchBlur}
+            placeholder="Pesquisar..."
+            w={500}
+            mr={2}
+          />
+          <IconButton _hover={{ bg: '#ED8936' }} bg="none" wd aria-label="Search database" icon={<Search2Icon />} />
+        </Button>
+      </Box>
+
+      <Box marginRight={20} display={{ base: 'none', md: 'block' }}>
+        <Button color="black" _hover={{ bg: '#ED8936' }} bg="none" colorScheme="teal" mr={4}>
+          Início
+        </Button>
+        <Button color="black" _hover={{ bg: '#ED8936' }} bg="none" colorScheme="teal" mr={4}>
+          Sobre
+        </Button>
+        <Button color="black" _hover={{ bg: '#ED8936' }} leftIcon={<EmailIcon />} align="center" bg="none" colorScheme="teal" mr={1} onClick={handleContatoClick}>
+          Contato
+        </Button>
+      </Box>
+
+      <Box display={{ base: 'block', md: 'none' }}>
+        <Button onClick={onToggle} colorScheme="teal">
+          Menu
+        </Button>
+      </Box>
+
+      <Collapse in={isOpen}>
+        <MobileMenu />
+      </Collapse>
+      <Sidebar />
+    </Flex>
+  );
 }
-export default header;
+
+export default Header;
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
-import { Button, Flex } from '@chakra-ui/react';
-import { IconButton } from '@chakra-ui/react'
-import { IoMdShare } from "react-icons/io";
+import React, { useState, useEffect } from 'react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { IoMdShare } from 'react-icons/io';
+import { AiFillLike } from 'react-icons/ai';
+import { IoIosInformationCircle } from "react-icons/io";
 
 
 function Sidebar() {
   const [interacao1, setInteracao1] = useState(false);
-  const [interacao2, setInteracao2] = useState(false);
+  const [interacao2, setInteracao2] = useState(0); // Inicializa a contagem de curtidas
   const [interacao3, setInteracao3] = useState(false);
 
-  const handleInteracao1 = async () => {
-  
+  useEffect(() => {
+    // Carrega a contagem de curtidas do localStorage ao carregar o componente
+    const curtidasSalvas = JSON.parse(localStorage.getItem('curtidas')) || 0;
+    setInteracao2(curtidasSalvas);
+  }, []);
 
+  const handleInteracao1 = async () => {
     try {
       const url = window.location.href;
       await navigator.clipboard.writeText(url);
@@ -23,20 +29,34 @@ function Sidebar() {
 
   const handleInteracao2 = () => {
     setInteracao1(false);
-    setInteracao2(!interacao2);
+    // Incrementa a contagem de curtidas
+    const novaContagem = interacao2 + 1;
+    setInteracao2(novaContagem);
+    // Salva a contagem de curtidas no localStorage
+    localStorage.setItem('curtidas', JSON.stringify(novaContagem));
     setInteracao3(false);
   };
 
   const handleInteracao3 = () => {
     setInteracao1(false);
-    setInteracao2(false);
+    setInteracao2(0);
+    localStorage.removeItem('curtidas'); // Remove as curtidas do localStorage
     setInteracao3(!interacao3);
   };
 
   return (
-    <Flex w="80px" position="fixed" top="0" right="0" h="100%" bg="#F6AD55"  justifyContent={'space-between'} flexDirection={'column'} textAlign={'center'}>
+    <Flex
+      w="80px"
+      position="fixed"
+      top="0"
+      right="0"
+      h="100%"
+      bg="#F6AD55"
+      justifyContent={'space-between'}
+      flexDirection={'column'}
+      textAlign={'center'}
+    >
       <IconButton
-        
         marginTop={3}
         onClick={handleInteracao1}
         icon={<IoMdShare />}
@@ -46,26 +66,32 @@ function Sidebar() {
         bg={'none'}
         fontSize={'25px'}
       />
-      <Button
-        onClick={handleInteracao2}
-        variant="ghost"
-        w="100%"
-        p={4}
-        color="black"
-        bgColor={interacao2 ? 'blue.500' : 'transparent'}
-      >
-        Interacao 2
-      </Button>
-      <Button
+      <Flex _hover={{ bg: '#ED8936' }} bg={'none'} flexDirection="column" alignItems="center">
+        <IconButton
+          _hover={{bg:'none'}}
+          fontSize={'25px'}
+          icon={<AiFillLike />}
+          onClick={handleInteracao2}
+          variant="ghost"
+          w="100%"
+          p={4}
+          color="black"
+          
+        />
+        <Text fontWeight={'bold'} fontSize="sm" color="black">
+          {interacao2} 
+        </Text>
+      </Flex>
+      <IconButton
+      _hover={{bg:'none'}}
+        fontSize={'25px'}
+        icon={<IoIosInformationCircle />}
         onClick={handleInteracao3}
         variant="ghost"
         w="100%"
         p={4}
         color="black"
-        bgColor={interacao3 ? 'purple.500' : 'transparent'}
-      >
-        Interacao 3
-      </Button>
+      />
     </Flex>
   );
 }
